f740b1f21c4a12263d258b3c82bac8ea0e0b9a4f
Stijn Robben <98906222+stijn-robben@users.noreply.github.com>
2025-02-04 16:52:20 +0100

    Order en enum

--- Order.java
+++ Order.java
@@ -1,3 +1,32 @@
+import java.util.ArrayList;
+
 public class Order {
-    
+    int orderNr;
+    boolean isStudentOrder;
+    ArrayList<MovieTicket> tickets = new ArrayList<MovieTicket>(); 
+
+
+    public Order(int orderNr, boolean isStudentOrder) {
+        this.orderNr = orderNr;
+        this.isStudentOrder = isStudentOrder;
+    }
+
+    public int getOrderNr() {
+        return orderNr;
+    }
+
+    public void addSeatReservation(MovieTicket movieTicket) {
+        tickets.add(movieTicket); 
+    }
+
+    public double calculatePrice() {
+        return 0.0; //add logic
+    }
+
+    public void export(TicketExportFormat exportFormat){
+        //add logic
+    }
+
+
+
 }
--- TicketExportFormat.java
+++ TicketExportFormat.java
@@ -1,3 +1,3 @@
-public class TicketExportFormat {
-    
+enum TicketExportFormat {
+    PLAINTEXT, JSON    
 }
