4dc6891f7e0b72f05c0c5183b1d411e65eaf25eb
Steen010 <kjh.vansteen@student.avans.nl>
2025-02-04 22:03:24 +0100

    Lesson 1

--- Movie.java
+++ Movie.java
@@ -1,3 +1,27 @@
 public class Movie {
-    
+    private String title;
+    private MovieScreening[] screenings;
+
+    public Movie(String title) {
+        this.title = title;
+    }
+
+    public void addScreening(MovieScreening screening) {
+        // Add the screening to the array of screenings
+        if (screenings == null) {
+            screenings = new MovieScreening[1];
+            screenings[0] = screening;
+        } else {
+            MovieScreening[] newScreenings = new MovieScreening[screenings.length + 1];
+            for (int i = 0; i < screenings.length; i++) {
+                newScreenings[i] = screenings[i];
+            }
+            newScreenings[screenings.length] = screening;
+            screenings = newScreenings;
+        }
+    }
+
+    public String toString() {
+        return title;
+    }
 }
--- MovieScreening.java
+++ MovieScreening.java
@@ -1,3 +1,21 @@
+import java.time.LocalDateTime;
+
 public class MovieScreening {
-    
+    private LocalDateTime dateAndTime;
+    private double pricePerSeat;
+    private Movie movie;
+
+    public MovieScreening(Movie movie, LocalDateTime dateAndTime, double pricePerSeat) {
+        this.dateAndTime = dateAndTime;
+        this.pricePerSeat = pricePerSeat;
+        this.movie = movie;
+    }
+
+    public double getPricePerSeat() {
+        return pricePerSeat;
+    }
+
+    public String toString() {
+        return "Movie: " + movie + ", Date and time: " + dateAndTime + ", price per seat: " + pricePerSeat;
+    }
 }
