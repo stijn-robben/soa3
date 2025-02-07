bb42e25917af259ffba018bee861ab21d23eccd7
Stijn Robben <98906222+stijn-robben@users.noreply.github.com>
2025-02-07 13:10:51 +0100

    Het werkt

---
+++ src/test/java/com/soa/demo/OrderTest.java
@@ -0,0 +1,14 @@
+package com.soa.demo;
+
+import org.junit.jupiter.api.Test;
+
+import com.soa.MovieTicket;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Arrays;
+
+public class OrderTest {
+
+    
+}
