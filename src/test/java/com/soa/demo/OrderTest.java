1ea08e917805fc394c3326c0b94f09a174d37a8a
Steen010 <kjh.vansteen@student.avans.nl>
2025-02-07 17:32:24 +0100

    yml

--- src/test/java/com/soa/demo/OrderTest.java
+++ src/test/java/com/soa/demo/OrderTest.java
@@ -5,10 +5,14 @@ import org.junit.jupiter.api.Test;
 import com.soa.MovieTicket;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Arrays;
 
 public class OrderTest {
 
-    
+    @Test
+    void testAlwaysTrue() {
+        assertTrue(true, "This test will always pass");
+    }
 }
